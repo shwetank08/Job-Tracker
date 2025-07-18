ec5b0733b2dbe6ed42005e5a7aafc0857fed73c6
Shwetank <shwetanksingh.in@gmail.com>
2025-07-18 13:43:36 +0530

    signup page update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 const App = () => {
   return (
     <>
-      <SignIn/>
+      <SignUp/>
     </>
   )
 }
